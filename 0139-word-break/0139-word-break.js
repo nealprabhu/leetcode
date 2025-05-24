26e705d5ef9d2089474faef876a6ed3c9a47fc43
nealprabhu <nealprabhu@gmail.com>
2025-05-24 18:42:13 +0530

    Time: 2 ms (90.94%), Space: 56.2 MB (56.41%) - LeetHub

---
+++ 0139-word-break/0139-word-break.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+// var wordBreak = function(s, wordDict) {
+//         const wordSet = new Set(wordDict);
+//     const dp = Array(s.length + 1).fill(false);
+//     dp[0] = true;
+
+//     for (let i = 1; i <= s.length; i++) {
+//         for (let j = 0; j < i; j++) {
+//             if (dp[j] && wordSet.has(s.substring(j, i))) {
+//                 dp[i] = true;
+//                 break;
+//             }
+//         }
+//     }
+
+//     return dp[s.length];
+    
+// };
+var wordBreak = function(s, wordDict) {
+    let dp = new Array(s.length + 1).fill(false);
+    dp[0] = true;
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let w of wordDict) {
+            let start = i - w.length;
+            if (start >= 0 && dp[start] && s.substring(start, i) === w) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[s.length];    
+};

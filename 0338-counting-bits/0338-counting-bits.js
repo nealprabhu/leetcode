3c88ccd4123c6f4d51b5a61ff2615fd94bc44728
nealprabhu <nealprabhu@gmail.com>
2025-06-01 18:56:53 +0530

    Time: 18 ms (22.13%), Space: 67.9 MB (7.71%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,10 +3,12 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-let val = new Array(n+1).fill(0);
-    for(let i=1;i<=n;i++){
+    let val= [];
+// let val = new Array(n+1).fill(0);
+    for(let i=0;i<=n;i++){
         let data = i.toString(2).split("").filter((res)=>res != "0");
-        val[i] = data.length;
+        // val[i] = data.length;
+        val.push(data.length);
     }
     return val;
 };

99db17112128597231f432619903846771571c2d
nealprabhu <nealprabhu@gmail.com>
2025-07-05 22:12:02 +0530

    Update 1524-string-matching-in-an-array.js

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.js
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.js
@@ -14,3 +14,18 @@ var stringMatching = function(words) {
   return res;
     
 };
+
+
+// alternane sol
+// var stringMatching = function(words) {
+//     const result = [];
+//     for (let i = 0; i < words.length; i++) {
+//         for (let j = 0; j < words.length; j++) {
+//             if (i !== j && words[j].includes(words[i])) {
+//                 result.push(words[i]);
+//                 break;
+//             }
+//         }
+//     }
+//     return result;
+// };
